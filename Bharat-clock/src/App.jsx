cac8d107cc655bdfdad808a625f5f7ed38746e7c
Vivek-Chand <154611649+Vivek-Chand@users.noreply.github.com>
2024-03-21 13:12:35 +0530

    Add files via upload

---
+++ Bharat-clock/src/App.jsx
@@ -0,0 +1,14 @@
+import ClockHeading from "./ClockHeading";
+import ClockSlogan from "./ClockSlogan";
+import CurrentTime from "./CurrentTime";
+
+import "./App.css"
+function App(){
+  return ( <div>
+   <ClockHeading/>
+   <ClockSlogan/>
+   <CurrentTime/>
+
+  </div> );
+}
+export default App;
